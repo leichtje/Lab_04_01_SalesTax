9f3f350342d490d33f47c5915ca41c4f6db73fb3
leichtje <113708612+leichtje@users.noreply.github.com>
2022-09-18 10:02:27 -0400

    Initial commit

---
+++ src/SalesTax.java
@@ -0,0 +1,16 @@
+public class SalesTax {
+    public static void main(String[] args) {
+        double SALES_TAX=.05;
+        double purchasePrice=650;
+        double calculatedTax=0;
+        double totalPrice=0;
+
+        calculatedTax=purchasePrice*SALES_TAX;
+        totalPrice=purchasePrice+calculatedTax;
+
+        System.out.println("The price of the purchase before tax is: "+ purchasePrice);
+        System.out.println("The purchases Sales tax is: "+ calculatedTax);
+        System.out.println("The total price of the purchase is: "+ totalPrice);
+    }
+}
+
